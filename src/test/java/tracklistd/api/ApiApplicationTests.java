1da5bf2c9aef35a35c082fe9fab605a077f62e74
João Pedro <joao.guadalupe06@gmail.com>
2026-05-22 09:57:12 -0300

    feat: configuração springboot

---
+++ src/test/java/tracklistd/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tracklistd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
