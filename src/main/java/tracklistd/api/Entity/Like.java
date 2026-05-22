cfdeee0a448e4a6928fb8dcf75e35f50e6e765c2
João Pedro <joao.guadalupe06@gmail.com>
2026-05-22 15:25:50 -0300

    feat: like entity and dto

--- src/main/java/tracklistd/api/Entity/Like.java
+++ src/main/java/tracklistd/api/Entity/Like.java
@@ -1,5 +1,29 @@
 package tracklistd.api.Entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;
+import tracklistd.api.Entity.Interfaces.Publication;
+
+@Entity
 public class Like {
 
+    // id - chave primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // referencia ao usuario que curtiu
+    @ManyToOne
+    private User user;
+
+    // referencia a publicação curtida
+    @ManyToOne
+    private Publication publication;
+
+    // referencia a data da publicação (criada automaticamente)
+    @CreationTimestamp
+    private LocalDateTime dateTime;
 }
