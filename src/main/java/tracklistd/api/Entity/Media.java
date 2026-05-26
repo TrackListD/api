13e5cc927c5651b79241cab2396612b99ba44433
Artsm12 <arthursouzamarques18@gmail.com>
2026-05-26 02:06:18 -0300

    feat: implementada lógica das classes Media e Music

--- src/main/java/tracklistd/api/Entity/Media.java
+++ src/main/java/tracklistd/api/Entity/Media.java
@@ -1,5 +1,44 @@
 package tracklistd.api.Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "type_media", discriminatorType = DiscriminatorType.STRING)
+@NoArgsConstructor
 public abstract class Media {
 
+    @Id
+    protected Long spotifyID;
+
+    @Column(nullable = false, length = 150)
+    protected String title;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "media_artists", joinColumns = @JoinColumn(name = "media_id"), inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    @OrderColumn(name = "artist_order")
+    protected List<Artist> authors;
+
+    protected String coverURL;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "media_genres", joinColumns = @JoinColumn(name = "media_id"))
+    @Column(name = "genre")
+    protected Set<String> musicGenres;
+
+    @Column(name = "release_date")
+    protected LocalDate releaseDate;
+
+    public Artist getMainArtist() {
+        if (this.authors != null && !this.authors.isEmpty())
+            return this.authors.get(0);
+        return null;
+    }
 }
--- src/main/java/tracklistd/api/Entity/Music.java
+++ src/main/java/tracklistd/api/Entity/Music.java
@@ -1,5 +1,25 @@
 package tracklistd.api.Entity;
 
-public class Music {
+import java.time.Duration;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@DiscriminatorValue("music")
+@NoArgsConstructor
+public class Music extends Media {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    @Column(name = "duration")
+    private Duration duration;
+
+    public boolean isSingle() {
+        return this.album == null;
+    }
 }
