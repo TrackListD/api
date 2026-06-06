df903831b7ca10d0b58fef074e9a2ed6921b0efe
João Vitor Almeida <joaovitoraq2006@gmail.com>
2026-06-05 22:06:52 -0300

    feat(enums): implementar metodo matches no enum ListType

--- src/main/java/tracklistd/api/Entity/Enums/ListType.java
+++ src/main/java/tracklistd/api/Entity/Enums/ListType.java
@@ -1,6 +1,25 @@
 package tracklistd.api.Entity.Enums;
 
+import tracklistd.api.Entity.Album;
+import tracklistd.api.Entity.Media;
+import tracklistd.api.Entity.Music;
+
 public enum ListType {
-    ALBUM,
-    MUSIC
+    ALBUM{
+        @Override
+        public boolean matches(Media media) {
+            return media instanceof Album;
+        }
+
+    },
+    MUSIC{
+        @Override
+        public boolean matches(Media media) {
+            return media instanceof Music;
+        }
+
+    };
+
+    public abstract boolean matches(Media media);
+
 }
