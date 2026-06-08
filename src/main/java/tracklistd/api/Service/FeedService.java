9f231326f96c4c0660c37b0295d34a74a3af75de
João Pedro <joao.guadalupe06@gmail.com>
2026-06-08 20:10:10 -0300

    feat: feed service com social e global feed

--- src/main/java/tracklistd/api/Service/FeedService.java
+++ src/main/java/tracklistd/api/Service/FeedService.java
@@ -1,29 +1,35 @@
-/*
- * package tracklistd.api.Service;
- * 
- * import java.util.List;
- * 
- * import org.springframework.stereotype.Service;
- * 
- * import tracklistd.api.Entity.Publication;
- * import tracklistd.api.Entity.User;
- * import tracklistd.api.Repository.PublicationRepository;
- * 
- * @Service
- * public class FeedService {
- * 
- * private final PublicationRepository publicationRepository;
- * 
- * public FeedService(PublicationRepository publicationRepository) {
- * this.publicationRepository = publicationRepository;
- * }
- * 
- * public List<Publication> getFeed(User user) {
- * 
- * List<User> following = user.getFollowing();
- * 
- * return publicationRepository
- * .findByAuthorInOrderByCreatedAtDesc(following);
- * }
- * }
- */
+package tracklistd.api.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import tracklistd.api.Entity.Publication;
+import tracklistd.api.Entity.User;
+import tracklistd.api.Repository.PublicationRepository;
+
+@Service
+public class FeedService {
+
+    private final PublicationRepository publicationRepository;
+
+    public FeedService(PublicationRepository publicationRepository) {
+        this.publicationRepository = publicationRepository;
+    }
+
+    public List<Publication> getSocialFeed(User user) {
+
+        Set<User> following = user.getFollowing(); // pega todos os seguidos do usuario
+
+        return publicationRepository
+                .findByAuthorInOrderByCreatedAtDesc(following); // retorna todos os posts da lista "seguindo" do usuario
+    }
+
+    public List<Publication> getGlobalFeed() {
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
+
+        return publicationRepository.findTrending(oneWeekAgo);
+    }
+}
