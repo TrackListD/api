784368b5820346f9bd1f083a73896cf3450366a0
João Pedro <joao.guadalupe06@gmail.com>
2026-06-17 11:53:41 -0300

    feat: adaptação controllers de feed e like para swagger

--- src/main/java/tracklistd/api/Controller/LikeController.java
+++ src/main/java/tracklistd/api/Controller/LikeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import tracklistd.api.Dto.Like.LikeResponseDTO;
 import tracklistd.api.Dto.User.UserMinResponseDTO;
 import tracklistd.api.Entity.User;
@@ -17,6 +20,7 @@ import tracklistd.api.Service.LikeService;
 
 @RestController
 @RequestMapping("/api/publications")
+@Tag(name = "Curtidas", description = "Endpoints para gerenciar interações de curtidas em publicações")
 public class LikeController {
 
     private final LikeService likeService;
@@ -26,15 +30,19 @@ public class LikeController {
     }
 
     @PostMapping("/{id}/like")
-    public ResponseEntity<LikeResponseDTO> toggleLike(@PathVariable Long id, @AuthenticationPrincipal User user) {
+    @Operation(summary = "Alternar curtida (Like/Unlike)", description = "Adiciona ou remove a curtida do usuário autenticado na publicação informada no path. Exige Token Bearer.")
+    public ResponseEntity<LikeResponseDTO> toggleLike(
+            @Parameter(description = "ID da publicação a ser curtida/descurtida", example = "1") @PathVariable Long id,
+            @Parameter(hidden = true) @AuthenticationPrincipal User user) {
         LikeResponseDTO response = likeService.toggleLike(user, id);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}/likes")
-    public ResponseEntity<List<UserMinResponseDTO>> seeWhoLiked(@PathVariable Long id) {
+    @Operation(summary = "Listar usuários que curtiram", description = "Retorna uma lista simplificada de todos os usuários que deram um like na publicação correspondente. Rota pública.")
+    public ResponseEntity<List<UserMinResponseDTO>> seeWhoLiked(
+            @Parameter(description = "ID da publicação desejada", example = "1") @PathVariable Long id) {
         List<UserMinResponseDTO> usersWhoLiked = likeService.getWhoLiked(id);
         return ResponseEntity.ok(usersWhoLiked);
     }
-
 }
