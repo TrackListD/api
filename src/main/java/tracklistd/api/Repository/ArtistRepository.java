912e642bbd430595f2cea24b4f6e3fa0eb0c0d1f
Artsm12 <arthursouzamarques18@gmail.com>
2026-06-23 20:34:25 -0300

    feat: adicionei o método findArtistBySpotifyID na ArtistRepository

--- src/main/java/tracklistd/api/Repository/ArtistRepository.java
+++ src/main/java/tracklistd/api/Repository/ArtistRepository.java
@@ -1,5 +1,7 @@
 package tracklistd.api.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import tracklistd.api.Entity.Artist;
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
+    Optional<Artist> findArtistBySpotifyID(String SpotifyId);
 }
