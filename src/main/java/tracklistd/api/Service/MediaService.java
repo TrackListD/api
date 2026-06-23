cd0a9534812213f7606a703126e12825bba7f683
Artsm12 <arthursouzamarques18@gmail.com>
2026-06-23 20:34:25 -0300

    feat: adicionei o método findArtistBySpotifyID na ArtistRepository

--- src/main/java/tracklistd/api/Service/MediaService.java
+++ src/main/java/tracklistd/api/Service/MediaService.java
@@ -1,8 +1,9 @@
 package tracklistd.api.Service;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 import tracklistd.api.Entity.Media;
-import tracklistd.api.Exceptions.MediaExceptions.MediaException;
 import tracklistd.api.Exceptions.ResourceNotFoundException;
 import tracklistd.api.Repository.MediaRepository;
 
@@ -22,5 +23,13 @@ public class MediaService {
                 () -> new ResourceNotFoundException("Essa midia não existe")
         );
         return media;
+    }
+
+    @Transactional
+    public Media getOrCreateBySpotifyId(String spotifyId, String mediaType) {
+        
+        // 1. Tenta achar no MySQL local primeiro
+        return mediaRepository.findMediaBySpotifyID(spotifyId)
+                .orElseGet(() -> fabricarSalvarNovaMedia(spotifyId, mediaType));
     }
 }
