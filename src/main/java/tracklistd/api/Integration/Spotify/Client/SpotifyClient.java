7b95e332e2e09c006c9192875608b84757e1ca38
Artsm12 <arthursouzamarques18@gmail.com>
2026-06-24 18:59:18 -0300

    feat: método getArtistAlbums e SpotifyService trocado para SpotifySearchService

--- src/main/java/tracklistd/api/Integration/Spotify/Client/SpotifyClient.java
+++ src/main/java/tracklistd/api/Integration/Spotify/Client/SpotifyClient.java
@@ -1,5 +1,7 @@
 package tracklistd.api.Integration.Spotify.Client;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import tracklistd.api.Dto.SpotifyAPI.SpotifyAlbumContainerDTO;
 import tracklistd.api.Dto.SpotifyAPI.SpotifyAlbumResponseDTO;
 import tracklistd.api.Dto.SpotifyAPI.SpotifyArtistResponseDTO;
 import tracklistd.api.Dto.SpotifyAPI.SpotifyMusicResponseDTO;
@@ -77,5 +80,18 @@ public class SpotifyClient {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authManager.getAccessToken()); 
         return new HttpEntity<>(headers);
+    }
+
+    public List<SpotifyAlbumResponseDTO> getArtistAlbums(String artistSpotifyId) {
+        String url = SPOTIFY_API_URL + "/artists/" + artistSpotifyId + "/albums?include_groups=album,single&limit=20";
+
+        ResponseEntity<SpotifyAlbumContainerDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                createRequestWithToken(),
+                SpotifyAlbumContainerDTO.class
+        );
+    
+        return response.getBody() != null ? response.getBody().items() : List.of();
     }
 }
