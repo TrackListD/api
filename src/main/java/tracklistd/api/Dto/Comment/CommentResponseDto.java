e9cfc547d44bf56318ca60605a3f93d3f682c601
João Pedro <joao.guadalupe06@gmail.com>
2026-06-29 00:55:11 -0300

    fix: ajustes para tela de comentarios

--- src/main/java/tracklistd/api/Dto/Comment/CommentResponseDto.java
+++ src/main/java/tracklistd/api/Dto/Comment/CommentResponseDto.java
@@ -6,17 +6,15 @@ import java.time.LocalDateTime;
 @Schema(description = "DTO com os dados públicos de um comentário")
 public record CommentResponseDto(
 
-        Long id,
+                Long id,
 
-        @Schema(description = "ID da publicação à qual o comentário pertence", example = "1")
-        Long postId,
+                @Schema(description = "ID da publicação à qual o comentário pertence", example = "1") Long postId,
 
-        Long authorId,
+                Long authorId,
 
-        @Schema(description = "Texto do comentário", example = "Excelente análise!")
-        String text,
+                @Schema(description = "Texto do comentário", example = "Excelente análise!") String text,
 
-        LocalDateTime commentDate,
-        Long likeCount
-) {
+                LocalDateTime commentDate,
+                Long likeCount,
+                boolean likedByMe) {
 }
--- src/main/java/tracklistd/api/Repository/CommentRepository.java
+++ src/main/java/tracklistd/api/Repository/CommentRepository.java
@@ -1,27 +1,23 @@
 package tracklistd.api.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tracklistd.api.Entity.Comment;
-import tracklistd.api.Entity.Enums.Privacy;
-import tracklistd.api.Entity.MediaList;
 import tracklistd.api.Entity.Publication;
 import tracklistd.api.Entity.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // Métodos CRUD básicos já vêm inclusos (save, findById, findAll, delete, etc.)
 
-    //Método que retorna todas os comentarios feitos em um Publicação de um User
-    List<Comment> findAllByPost(Publication post);
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.likes WHERE c.post = :post")
+    List<Comment> findAllByPost(@Param("post") Publication post);
 
-    //Método que retorna a qtd de comentarios de um post
     Integer countByPost(Publication post);
 
-    //Método que retorna os comentarios de um User
-    List<Comment> getCommentsByAuthor(User author);
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.likes WHERE c.author = :author")
+    List<Comment> getCommentsByAuthor(@Param("author") User author);
 }
-
